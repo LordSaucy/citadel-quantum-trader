b319b30658444a9c030998fa0fdf1ae2be3e3486
lawful-banker <ladarren.rivers@proton.me>
2025-12-01 17:11:25 -0600

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+citadel_quantum_trader – production‑ready Python package
+"""
+
+__all__ = [
+    "config",
+    "risk_management",
+    "ultimate_confluence_system",
+    "advanced_execution_engine",
+    "main",
+]
